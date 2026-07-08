c08bfe56ad2333fb341de4435868a653543daf02
Abhishek Dhamala <dhamalaabhishek7@gmail.com>
2026-07-08 13:41:20 +0545

    feat(auth): wire auth routes into app router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/auth/LoginPage';
+import SignupPage from './pages/auth/SignupPage';
+import OTPPage from './pages/auth/OTPPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/verify-otp" element={<OTPPage />} />
       </Routes>
     </BrowserRouter>
   );
